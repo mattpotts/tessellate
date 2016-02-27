9e12d0fe83174038c5ee7dd0694e7a340e6bd2aa
Matt Potts <matt.potts@tripodsoftware.co.uk>
2016-02-27 09:21:12 +0000

    Each triangles color is selected randomly from a selection of 4 hardcoded colors

--- scripts/SquaresController.js
+++ scripts/SquaresController.js
@@ -3,20 +3,40 @@
  *
  * @constructor
  */
-function SquaresController($scope) {
+function SquaresController() {
   var vm = this;
 
   var squareCount = 49;
 
   vm.squares = [];
 
+  var colors = [
+    '#f3f3f3',
+    '#4d5e78',
+    '#7fb9df',
+    '#c85a80'
+  ];
+
+  /**
+   * Add a square of random colors to the squares array.
+   */
   var addSquare = function() {
     vm.squares.push([
-        '#f3f3f3',
-        '#4d5e78',
-        '#7fb9df',
-        '#c85a80'
+        randomColor(),
+        randomColor(),
+        randomColor(),
+        randomColor()
     ]);
+  };
+
+  /**
+   * Pick a random color from the colors array.
+   * @returns {string}
+   */
+  var randomColor = function () {
+    var max = colors.length;
+    var index = Math.floor(Math.random() * max);
+    return colors[index];
   };
 
   /*
